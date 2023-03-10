770e12528cc8f74f07677c295e84dc8f46e8a56d
Brad <bradveryard@gmail.com>
2023-03-10 11:51:22 +0800

    fix(git): Sort

--- src/version/git.js
+++ src/version/git.js
@@ -12,6 +12,14 @@ module.exports = class Git extends BaseVersioning {
       const prerelease = core.getBooleanInput('pre-release')
 
       gitSemverTags({ tagPrefix, skipUnstable: !prerelease }, async (err, tags) => {
+        if (tags.length > 0) {
+          tags.sort((a, b) => {
+            const aVersion = a.replace(tagPrefix, '');
+            const bVersion = b.replace(tagPrefix, '');
+
+            return bVersion.localeCompare(aVersion);
+          });
+        }
         this.oldVersion = tags.length > 0 ? tags.shift().replace(tagPrefix, '') : null
 
         // Get the new version
